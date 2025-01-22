cd0b874f166ccd1e40cfa0a39ace20541d2c7502
MIIGUEELC <miigueel9219@gmail.com>
2025-01-22 15:04:13 +0100

    probando thunks de bokins

--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -2,16 +2,28 @@ import { bodyInterface } from "../features/interfaces/interfaces";
 
 const urlBase = import.meta.env.VITE_API_URL;
 
-export const apiRequest = async (endpoint: string, method:string, body?:bodyInterface ) => {
-  const result = await fetch(`${urlBase}/${endpoint}`, {
-    method: `${method}`,
-    mode: 'cors',
-    headers: {
-      "Content-Type": "application/json",
-      token: `${localStorage.getItem('token')}`,
-    },
-     body: JSON.stringify(body)
-  });
-  const response = await result.json();
-  return response;
+export const apiRequest = async (endpoint: string, method: string, body?: bodyInterface) => {
+  try {
+    const result = await fetch(`${urlBase}/${endpoint}`, {
+      method: `${method}`,
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+        token: `${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify(body)
+    });
+
+    if (!result.ok) {
+      throw new Error(`Error: ${result.statusText}`);
+    }
+
+    const response = await result.json();
+    return response;
+
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    throw error;
+  }
 }
+
